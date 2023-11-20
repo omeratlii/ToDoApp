8cf4490d8e362a75a790439eba37a10a47774dc2
Ömer Atlı <atliomer41@gmail.com>
2023-11-20 16:25:36 +0300

    Proje Yüklendi
    Todoapp dosyaları yüklendi.

---
+++ ToDo/todoapp/src/CreateTask.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function CreateTask(props) {
+  const [task, setTask] = useState({
+    title: "",
+    content: "",
+  });
+  function detectChange(event) {
+    const { name, value } = event.target;
+
+    setTask((prevTask) => {
+      return {
+        ...prevTask,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitTask(event) {
+    props.onAdd(task);
+    setTask({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+  return (
+    <div className="todoDivArea">
+      <form className="todoDiv">
+        <input
+          type="text"
+          className="form-control todoText mb-3"
+          name="title"
+          value={task.title}
+          onChange={detectChange}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          value={task.content}
+          onChange={detectChange}
+          className="form-control todoText mb-3"
+          rows="3"
+          placeholder="What will you do?"
+        ></textarea>
+
+        <button className="btn btn-warning todoButton" onClick={submitTask}>
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
